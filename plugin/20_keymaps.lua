ec2231b81155d62e7606a1401cafe323939fc13d
Tom Jagus <168629725+tom-jagus@users.noreply.github.com>
2026-07-15 01:32:34 +0200

    Add LazyGit keymap

--- plugin/20_keymaps.lua
+++ plugin/20_keymaps.lua
@@ -176,6 +176,7 @@ nmap_leader('gl', '<Cmd>' .. git_log_cmd .. '<CR>',         'Log')
 nmap_leader('gL', '<Cmd>' .. git_log_buf_cmd .. '<CR>',     'Log buffer')
 nmap_leader('go', '<Cmd>lua MiniDiff.toggle_overlay()<CR>', 'Toggle overlay')
 nmap_leader('gs', '<Cmd>lua MiniGit.show_at_cursor()<CR>',  'Show at cursor')
+nmap_leader('gg', '<Cmd>LazyGit<CR>', "LazyGit")
 
 xmap_leader('gs', '<Cmd>lua MiniGit.show_at_cursor()<CR>', 'Show at selection')
 
