044f485a4de0b9883a0770535069f2b88cb500b2
Tom Jagus <168629725+tom-jagus@users.noreply.github.com>
2026-07-14 23:59:04 +0200

    Disable completion navigation with Tab S-Tab

--- plugin/30_mini.lua
+++ plugin/30_mini.lua
@@ -570,8 +570,8 @@ later(function() require('mini.jump2d').setup() end)
 later(function()
   require('mini.keymap').setup()
   -- Navigate 'mini.completion' menu with `<Tab>` /  `<S-Tab>`
-  MiniKeymap.map_multistep('i', '<Tab>', { 'pmenu_next' })
-  MiniKeymap.map_multistep('i', '<S-Tab>', { 'pmenu_prev' })
+  -- MiniKeymap.map_multistep('i', '<Tab>', { 'pmenu_next' })
+  -- MiniKeymap.map_multistep('i', '<S-Tab>', { 'pmenu_prev' })
   -- On `<CR>` try to accept current completion item, fall back to accounting
   -- for pairs from 'mini.pairs'
   MiniKeymap.map_multistep('i', '<CR>', { 'pmenu_accept', 'minipairs_cr' })
