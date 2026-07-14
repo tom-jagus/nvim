8383e8aedecb388d6c4f205f47f9aee6f3af2040
Tom Jagus <168629725+tom-jagus@users.noreply.github.com>
2026-07-14 23:44:48 +0200

    Disable mini.operators

--- plugin/30_mini.lua
+++ plugin/30_mini.lua
@@ -639,18 +639,18 @@ later(function() require('mini.move').setup() end)
 -- See also:
 -- - `:h MiniOperators-mappings` - overview of how mappings are created
 -- - `:h MiniOperators-overview` - overview of present operators
-later(function()
-  require('mini.operators').setup()
-
-  -- Create mappings for swapping adjacent arguments. Notes:
-  -- - Relies on `a` argument textobject from 'mini.ai'.
-  -- - It is not 100% reliable, but mostly works.
-  -- - It overrides `:h (` and `:h )`.
-  -- Explanation: `gx`-`ia`-`gx`-`ila` <=> exchange current and last argument
-  -- Usage: when on `a` in `(aa, bb)` press `)` followed by `(`.
-  vim.keymap.set('n', '(', 'gxiagxila', { remap = true, desc = 'Swap arg left' })
-  vim.keymap.set('n', ')', 'gxiagxina', { remap = true, desc = 'Swap arg right' })
-end)
+-- later(function()
+--   require('mini.operators').setup()
+--
+--   -- Create mappings for swapping adjacent arguments. Notes:
+--   -- - Relies on `a` argument textobject from 'mini.ai'.
+--   -- - It is not 100% reliable, but mostly works.
+--   -- - It overrides `:h (` and `:h )`.
+--   -- Explanation: `gx`-`ia`-`gx`-`ila` <=> exchange current and last argument
+--   -- Usage: when on `a` in `(aa, bb)` press `)` followed by `(`.
+--   vim.keymap.set('n', '(', 'gxiagxila', { remap = true, desc = 'Swap arg left' })
+--   vim.keymap.set('n', ')', 'gxiagxina', { remap = true, desc = 'Swap arg right' })
+-- end)
 
 -- Autopairs functionality. Insert pair when typing opening character and go over
 -- right character if it is already to cursor's right. Also provides mappings for
