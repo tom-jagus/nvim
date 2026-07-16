8510a4a5b20d5c89ba72c6431921462df76969fe
Tom Jagus <168629725+tom-jagus@users.noreply.github.com>
2026-07-16 23:26:00 +0200

    Enable start_lsp_server for mini.snippets

--- plugin/30_mini.lua
+++ plugin/30_mini.lua
@@ -762,7 +762,7 @@ later(function()
   -- By default snippets available at cursor are not shown as candidates in
   -- 'mini.completion' menu. This requires a dedicated in-process LSP server
   -- that will provide them. To have that, uncomment next line (use `gcc`).
-  -- MiniSnippets.start_lsp_server()
+  MiniSnippets.start_lsp_server()
 end)
 
 -- Split and join arguments (regions inside brackets between allowed separators).
