ca5647b4fc11d6cef92dfc8c5d7a9b10acff5d24
Tom Jagus <168629725+tom-jagus@users.noreply.github.com>
2026-07-27 00:17:24 +0200

    Add docs

--- lua/custom/vault_sync.lua
+++ lua/custom/vault_sync.lua
@@ -3,7 +3,7 @@ local M = {}
 local uv = vim.uv or vim.loop
 
 local defaults = {
-  vault = '~/vaults/tom-jagus',
+  vault = '~/vaults/second-brain',
   debounce_ms = 60 * 1000,
   sync_on_enter = true,
   notify_auto_success = false,
